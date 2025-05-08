ad48e1fb2e780885ffa4e16ca4c1f3f4545183ba
Clive GROSS <SESA368046@se.com>
2025-05-08 17:13:17 +1000

    update example scrips

--- scripts/make_astroschedule.py
+++ scripts/make_astroschedule.py
@@ -57,39 +57,39 @@ schedule.longitude = 153.028056 # longitude for Brisbane Australia CBD
 schedule.add_entry(time_ref="SunriseOffset", hour="0", minute="0", value="1") # start 45min before sunrise
 schedule.add_entry(time_ref="SunsetOffset", hour="0", minute="0", value=None) # end (return to default value) at 4:30 PM
 
+output_xml_2 = os.path.join(output_dir, 'schedule_2.xml')
+# Write the XML tree to a file
+schedule.write_xml(output_xml_2)
+
+print('\nNow create multiple schedules using same event entries but different geolocations and combine them into a single XML file')
 schedules = []
 
 # Make Brisbane sunrise/sunset schedule
 schedule.schedule_name = "Brisbane"
-output_xml = os.path.join(output_dir, schedule.schedule_name+' sunsrise sunset schedule.xml')
 schedule.latitude = -27.467778 # latitude for Brisbane Australia CBD 
 schedule.longitude = 153.028056 # longitude for Brisbane Australia CBD
 schedules.append(schedule.schedule)
 
 # Make Berlin sunrise/sunset schedule
 schedule.schedule_name = "Berlin"
-output_xml = os.path.join(output_dir, schedule.schedule_name+' sunsrise sunset schedule.xml')
 schedule.latitude = 52.520008 # latitude for Berlin
 schedule.longitude = 13.404954 # longitude for Berlin
 schedules.append(schedule.schedule)
 
 # Make Andover sunrise/sunset schedule
 schedule.schedule_name = "Andover"
-output_xml = os.path.join(output_dir, schedule.schedule_name+' sunsrise sunset schedule.xml')
 schedule.latitude = 42.656029 # latitude for Andover
 schedule.longitude = -71.157059 # longitude for Andover
 schedules.append(schedule.schedule)
 
 # Make Singapore sunrise/sunset schedule
 schedule.schedule_name = "Singapore"
-output_xml = os.path.join(output_dir, schedule.schedule_name+' sunsrise sunset schedule.xml')
 schedule.latitude = 1.290270 # latitude for Singapore
 schedule.longitude = 103.851959 # longitude for Singapore
 schedules.append(schedule.schedule)
 
 # Make Paris sunrise/sunset schedule
 schedule.schedule_name = "Paris"
-output_xml = os.path.join(output_dir, schedule.schedule_name+' sunsrise sunset schedule.xml')
 schedule.latitude = 48.864716 # latitude for Paris
 schedule.longitude = 2.349014 # longitude for Paris
 schedules.append(schedule.schedule)
